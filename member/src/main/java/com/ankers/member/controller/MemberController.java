ed6caa1317b1710214c548e041d1a7488c6fda0e
yuyayong <15189120919@163.com>
2024-12-28 22:46:29 +0800

    完成jwtUtil创建token，完成登录注册

--- member/src/main/java/com/ankers/member/controller/MemberController.java
+++ member/src/main/java/com/ankers/member/controller/MemberController.java
@@ -1,14 +1,14 @@
 package com.ankers.member.controller;
 
 import com.ankers.common.resp.CommonResp;
+import com.ankers.member.req.MemberLoginReq;
 import com.ankers.member.req.MemberRegisterReq;
+import com.ankers.member.req.MemberSendCodeReq;
+import com.ankers.member.resp.MemberLoginResp;
 import com.ankers.member.service.MemberService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member")
@@ -34,5 +34,17 @@ public class MemberController {
         resp.setSuccess(true);
         resp.setMessage("查询成功");
         return resp;
+    }
+
+    @PostMapping("/send-code")
+    public CommonResp<Long> sendCode(@Valid @RequestBody MemberSendCodeReq req) {
+        memberService.sendCode(req);
+        return new CommonResp<>();
+    }
+
+    @PostMapping("/login")
+    public CommonResp<MemberLoginResp> login(@Valid @RequestBody MemberLoginReq req) {
+        MemberLoginResp resp = memberService.login(req);
+        return new CommonResp<>(resp);
     }
 }
--- member/src/main/java/com/ankers/member/service/MemberService.java
+++ member/src/main/java/com/ankers/member/service/MemberService.java
@@ -1,14 +1,30 @@
 package com.ankers.member.service;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjectUtil;
+import com.ankers.common.exception.BusinessException;
+import com.ankers.common.exception.BusinessExceptionEnum;
+import com.ankers.common.util.JwtUtil;
 import com.ankers.common.util.SnowUtil;
 import com.ankers.member.domain.Member;
+import com.ankers.member.domain.MemberExample;
 import com.ankers.member.mapper.MemberMapper;
+import com.ankers.member.req.MemberLoginReq;
 import com.ankers.member.req.MemberRegisterReq;
+import com.ankers.member.req.MemberSendCodeReq;
+import com.ankers.member.resp.MemberLoginResp;
 import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MemberService.class);
 
     @Resource
     private MemberMapper memberMapper;
@@ -23,5 +39,60 @@ public class MemberService {
         member.setMobile(mobile.getMobile());
         memberMapper.insert(member);
         return member.getId();
+    }
+
+    public void sendCode(MemberSendCodeReq req) {
+        String mobile = req.getMobile();
+        Member memberDB = selectByMobile(mobile);
+
+        if (ObjectUtil.isNull(memberDB)) {
+            LOG.info("手机号不存在，插入一条记录");
+            Member member = new Member();
+            member.setId(SnowUtil.getSnowflakeNextId());
+            member.setMobile(mobile);
+            memberMapper.insert(member);
+        } else {
+            LOG.info("手机号存在，不插入记录");
+        }
+//        String s = RandomUtil.randomString(4);
+        String code = "8888";
+        LOG.info("生成短信验证码：{}", code);
+
+        // 保存短信记录表：手机号，短信验证码，有效期，是否已使用，业务类型，发送时间，使用时间
+        LOG.info("保存短信记录表");
+
+        // 对接短信通道，发送短信
+        LOG.info("对接短信通道");
+    }
+
+    public MemberLoginResp login(MemberLoginReq req) {
+        String mobile = req.getMobile();
+        Member memberDB = selectByMobile(mobile);
+
+        if (ObjectUtil.isNull(memberDB)) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_NOT_EXIST);
+        }
+
+        String code = req.getCode();
+        // 校验短信验证码
+        if (!"8888".equals(code)) {
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_CODE_ERROR);
+        }
+
+        MemberLoginResp memberLoginResp = BeanUtil.copyProperties(memberDB, MemberLoginResp.class);
+        String token = JwtUtil.createToken(memberDB.getId(), mobile);
+        memberLoginResp.setToken(token);
+        return memberLoginResp;
+    }
+
+    private Member selectByMobile(String mobile) {
+        MemberExample example = new MemberExample();
+        example.createCriteria().andMobileEqualTo(mobile);
+        List<Member> members = memberMapper.selectByExample(example);
+        if (CollUtil.isEmpty(members)) {
+            return null;
+        } else {
+            return members.get(0);
+        }
     }
 }

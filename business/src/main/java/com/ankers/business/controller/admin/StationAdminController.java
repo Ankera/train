0c3d4ecbc3504a5566c6e12dd4692cb24f3d0a39
yuyayong <15189120919@163.com>
2025-01-09 00:08:40 +0800

    增加station生成器,修改adminFTL

---
+++ business/src/main/java/com/ankers/business/controller/admin/StationAdminController.java
@@ -0,0 +1,38 @@
+package com.ankers.business.controller.admin;
+
+import com.ankers.common.resp.CommonResp;
+import com.ankers.common.resp.PageResp;
+import com.ankers.business.req.StationQueryReq;
+import com.ankers.business.req.StationSaveReq;
+import com.ankers.business.resp.StationQueryResp;
+import com.ankers.business.service.StationService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/station")
+public class StationAdminController {
+
+    @Resource
+    private StationService stationService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody StationSaveReq req) {
+        stationService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<StationQueryResp>> queryList(@Valid StationQueryReq req) {
+        PageResp<StationQueryResp> list = stationService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        stationService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ business/src/main/java/com/ankers/business/req/StationQueryReq.java
@@ -0,0 +1,12 @@
+package com.ankers.business.req;
+
+import com.ankers.common.req.PageReq;
+
+public class StationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "StationQueryReq{" +
+                "} " + super.toString();
+    }
+}

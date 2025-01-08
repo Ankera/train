88e68ac93de9c1310229b8f9f0115e0d9a274f08
yuyayong <15189120919@163.com>
2025-01-08 10:08:11 +0800

    新增business模块

---
+++ business/src/main/java/com/ankers/business/config/BusinessApplication.java
@@ -0,0 +1,24 @@
+package com.ankers.business.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.ankers")
+@MapperScan("com.ankers.business.mapper")
+public class BusinessApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BusinessApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功!!!");
+        LOG.info("地址http://localhost:{}", env.getProperty("server.port"));
+    }
+}

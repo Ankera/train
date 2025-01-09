d5eea83d5f9f3edbb4c65e0f0016acda3cddf7d4
yuyayong <15189120919@163.com>
2025-01-09 23:59:34 +0800

    增加火车车站表，生成相关代码

---
+++ business/src/main/java/com/ankers/business/req/TrainStationQueryReq.java
@@ -0,0 +1,12 @@
+package com.ankers.business.req;
+
+import com.ankers.common.req.PageReq;
+
+public class TrainStationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainStationQueryReq{" +
+                "} " + super.toString();
+    }
+}

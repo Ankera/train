68981babc0de8da95ed41c369294cbbfe67e1930
yuyayong <15189120919@163.com>
2025-01-13 23:54:47 +0800

    禁止任务并发执行

--- batch/src/main/java/com/ankers/batch/job/TestJob.java
+++ batch/src/main/java/com/ankers/batch/job/TestJob.java
@@ -1,16 +1,18 @@
 package com.ankers.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         System.out.println("TestJob TEST开始");
         Long start = System.currentTimeMillis();
          try {
-             Thread.sleep(3000);
+             Thread.sleep(10000);
              Long duration = System.currentTimeMillis() - start;
              System.out.println("duration==>" + duration);
          } catch (InterruptedException e) {

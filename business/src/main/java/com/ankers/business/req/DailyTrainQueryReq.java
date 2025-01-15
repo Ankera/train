7fa12aa1c2a9813d54ae1675f7da90cd2c4176a7
yuyayong <15189120919@163.com>
2025-01-16 00:06:37 +0800

    为每日车次页面增加日期和车次查询条件。注意：GET请求的日期是？拼接在URL里的，需要用spring自带的DateTimeFormat

--- business/src/main/java/com/ankers/business/req/DailyTrainQueryReq.java
+++ business/src/main/java/com/ankers/business/req/DailyTrainQueryReq.java
@@ -1,12 +1,37 @@
 package com.ankers.business.req;
 
 import com.ankers.common.req.PageReq;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 public class DailyTrainQueryReq extends PageReq {
 
-    @Override
-    public String toString() {
-        return "DailyTrainQueryReq{" +
-                "} " + super.toString();
+    /**
+     * get 请求用 DateTimeFormat
+     * post 请求用 JsonFormat
+     */
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private Date date;
+
+    private String code;
+
+    public Date getDate() {
+        return date;
     }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+
 }

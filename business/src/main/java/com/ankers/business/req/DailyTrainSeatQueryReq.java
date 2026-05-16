9ff0adc7ae7e876d79530a749d8e25f3f07fda88
yu <222@qq.com>
2026-05-16 12:20:39 +0800

    增加每日座位

---
+++ business/src/main/java/com/ankers/business/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package com.ankers.business.req;
+
+import com.ankers.common.req.PageReq;
+
+public class DailyTrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
